badf423ab39a0562533c1131592893fd0a0fcbb4
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-10-31 18:53:46 +0530

    fixed pip3

--- packer/scripts/_ssh.sh
+++ packer/scripts/_ssh.sh
@@ -8,4 +8,4 @@ sudo mv -f /tmp/sshd_config /etc/ssh/sshd_config
 echo 'DevOps321' | sudo passwd --stdin root
 sudo service sshd restart
 
-sudo yum install -y python3.11 wget vim unzip python-pip3
+sudo yum install -y python3.11 wget vim unzip python3-pip
