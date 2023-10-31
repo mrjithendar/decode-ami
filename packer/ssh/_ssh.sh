6ac7b8b9b55f8d991f551c968d52b662090e2fcb
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-10-31 19:50:05 +0530

    fixed ssh

--- packer/ssh/_ssh.sh
+++ packer/ssh/_ssh.sh
@@ -6,5 +6,3 @@ echo 'DevOps321' | sudo passwd --stdin root
 chmod 700 /root/.ssh
 chmod 600 /root/.ssh/authorized_keys
 sudo service sshd restart
-
-sudo yum install -y python3.11 wget vim unzip python3-pip
