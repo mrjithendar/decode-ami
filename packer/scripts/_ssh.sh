50c582c03139931bb710cc65000191c0b1226a3b
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2023-10-31 19:10:24 +0530

    updaed ssh permissions

--- packer/scripts/_ssh.sh
+++ packer/scripts/_ssh.sh
@@ -6,6 +6,8 @@ sudo rm -rf /etc/ssh/sshd_config
 sudo mv -vf /tmp/sshd_config /etc/ssh/sshd_config
 
 echo 'DevOps321' | sudo passwd --stdin root
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/authorized_keys
 sudo service sshd restart
 
 sudo yum install -y python3.11 wget vim unzip python3-pip
